c7d7d4268bc2bce31d35879ba88c4b3f3c8f22b8
leandrop <leandro.p.n96@gmail.com>
2022-02-10 11:45:41 -0300

    feat: :sparkles: Implementation DSR and Barra de cookies

---
+++ nuxt/src/components/Tutorial.vue
@@ -0,0 +1,34 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div style="background-color: #2d0073 " class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div  class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <img src="https://certsysti.sharepoint.com/sites/portal/SiteAssets/colorido-claro.png" alt="">
+      <div style="display: flex; justify-content:center;  align-item: center " class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+       <a href="/dsr" class="button">
+         ACESSAR DSR
+       </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
+<style>
+.button{
+  background: #2196F3;
+  width: 300px;
+  font-size: 20px;
+  height: 40px;
+  border: 1px solid black;
+  border-radius: 5px;
+  font-weight: 400;
+  color:aliceblue;
+  display: flex;
+  justify-content: center;
+}
+</style>
